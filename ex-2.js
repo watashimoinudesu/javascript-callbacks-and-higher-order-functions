2c5d7822cab10772c1d1006353e212c962d885e9
Anuson <anusornthongpaenchai@gmail.com>
2025-12-16 13:11:35 +0700

    Assignment_update

--- ex-2.js
+++ ex-2.js
@@ -5,16 +5,37 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 function checkGradeOver70(score) {
   // Start coding here
+  if (score > 70) {
+    return true
+  } else {
+    return false
+  };
 }
+
+console.log(checkGradeOver70(10));
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  let result;
+  for (let grade of array){
+    if (operation(grade) === true){
+      count++;
+    }
+  }
+  if (count >= 5) {
+    result = true;
+  } else{
+    result = false;
+  }
+  return result
 }
 
+
 // Using `atLeastFive` function here
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkGradeOver70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkGradeOver70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkGradeOver70);
 
 console.log(scoreRoom1Result); //true
 console.log(scoreRoom2Result); //false
@@ -24,8 +45,10 @@ console.log(scoreRoom3Result); //false
 ====================================
 
 1. ใน Exercise นี้ ฟังก์ชันใดเป็น Callback Function?
+    function checkGradeOver70 เป็น Callback Function จุดสังเกตคือเป็น function ที่ รอการเรียกใช้จาก function อื่น
 
 2. ใน Exercise นี้ ฟังก์ชันใดเป็น Higher Order Function?
+  function atLeastFive เป็น Higher Order Function จุดสังเกตคือเป็น function ที่เอาไว้จัดการกับ function อื่น
 
 ====================================
 */
